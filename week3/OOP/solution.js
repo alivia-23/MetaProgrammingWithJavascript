fe53b0fb320133ee4b505e732e289b4aec541160
alivia-23 <guin.al@husky.neu.edu>
2024-05-03 16:15:44 -0700

    assignment

---
+++ week3/OOP/solution.js
@@ -0,0 +1,42 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy() {
+    for (let product of dairy) {
+        console.log(product);
+    }
+}
+
+logDairy();
+
+
+// Task 2
+const animal = {
+
+    canJump: true
+    
+};
+    
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (let prop of Object.keys(bird)) {
+        console.log(`${prop} : ${bird[prop]}`)
+    }
+}
+
+birdCan();
+console.log("************************************")
+// Task 3
+
+function animalCan() {
+    for (let prop in bird) {
+        console.log(`${prop} : ${bird[prop]}`)
+    }
+}
+animalCan()
+
